0b5326bc6e9665468365edaae7aeaeb40a42d647
user <aleksey.slonov@mail.ru>
2020-03-08 14:08:21 +0600

    add Models.News

--- PUM_Site/PUM_Site/models.py
+++ PUM_Site/PUM_Site/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class News(models.Model):
+    date = models.DateTimeField()
+    header = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
